5716556e81ebca08f61604e18c2273d0e2a39af6
arnaudstiegler <arnaudstiegler@129.104.96.198>
2017-03-24 15:11:13 +0100

    Arnaud
    Test commit encore....

--- StreetArtProject/content/content_submit.php
+++ StreetArtProject/content/content_submit.php
@@ -1,11 +1,5 @@
 <html>
 
-<!--<form method="POST" action="upload.php" enctype="multipart/form-data">
-      On limite le fichier à 100Ko 
-     <input type="hidden" name="MAX_FILE_SIZE" value="100000">
-     Fichier : <input type="file" name="avatar">
-     <input type="submit" name="envoyer" value="Envoyer le fichier">
-</form>-->
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
    <input type="file" name="fichier"/>
