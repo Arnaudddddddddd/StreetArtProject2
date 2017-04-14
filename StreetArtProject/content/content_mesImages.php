937ec855de9f4df96e5ba77a4439c475d0394086
Vardoom <paul.vardon@polytechnique.edu>
2017-04-14 18:56:29 +0200

    Vardon Mes Images

--- StreetArtProject/content/content_mesImages.php
+++ StreetArtProject/content/content_mesImages.php
@@ -7,29 +7,33 @@ if ($pageTitle == "Mes images") {
         $resultat = Image::getImageUtilisateur($dbh, $utilisateur);
 
         if (isset($resultat)) {
-            var_dump($resultat);
-            echo '<div id="gallery1">';
+            //var_dump($resultat);
+            echo '<div id="gallery1" style="margin:0px auto; display:none;">';
             foreach ($resultat as $name) {
-                print($name);
+                //print($name);
                 echo <<<CHAINE_DE_FIN
-                <img alt="$name"
-                    src="miniatures/mini_$name.jpg"
-                    data-image="images/$name.jpg"
-                    data-description="This is $name"
-                >
+                    <a ref="http://google.fr">
+                        <img alt="$name"
+                            src="miniatures/mini_$name.jpg"
+                            data-image="images/$name.jpg"
+                            data-description="This is $name"
+                        >
+                    </a>
 CHAINE_DE_FIN;
             }
             echo <<<CHAINE_DE_FIN
-        </div>
+                </div>
 
-<script type="text/javascript">
-    jQuery(document).ready(function () {
-        jQuery("#gallery1").unitegallery({
-            gallery_theme: "tiles",
-            tiles_type: "nested"
-        });
-    });
-</script>
+                <script type="text/javascript">
+                    jQuery(document).ready(function () {
+                        jQuery("#gallery1").unitegallery({
+                            gallery_theme: "tiles",
+                            tiles_type: "nested",
+                            tile_link_newpage:false,
+                            tile_show_link_icon: true
+                        });
+                    });
+                </script>
 CHAINE_DE_FIN;
         } else {
             echo "<div>Vous n'avez pas soumis de photos</div>";
@@ -37,28 +41,32 @@ CHAINE_DE_FIN;
     }
     if ($_SESSION['admin'] == true) {
         $resultat = Image::getAllImages($dbh);
-        var_dump($resultat);
+        //var_dump($resultat);
         echo '<div id="gallery1" style="margin:0px auto; display:none;">';
         foreach ($resultat as $name) {
             echo <<<CHAINE_DE_FIN
-        <img alt="$name"
-         src="miniatures/mini_$name.jpg"
-         data-image="images/$name.jpg"
-         data-description="This is $name"
-        >
+                    <a ref="http://google.fr">
+                        <img alt="$name"
+                            src="miniatures/mini_$name.jpg"
+                            data-image="images/$name.jpg"
+                            data-description="This is $name"
+                        >
+                    </a>
 CHAINE_DE_FIN;
-        }
-        echo <<<CHAINE_DE_FIN
-        </div>
+            }
+            echo <<<CHAINE_DE_FIN
+                </div>
 
-<script type="text/javascript">
-    jQuery(document).ready(function () {
-        jQuery("#gallery1").unitegallery({
-            gallery_theme: "tiles",
-            tiles_type: "nested"
-        });
-    });
-</script>
+                <script type="text/javascript">
+                    jQuery(document).ready(function () {
+                        jQuery("#gallery1").unitegallery({
+                            gallery_theme: "tiles",
+                            tiles_type: "nested",
+                            tile_link_newpage:false,
+                            tile_show_link_icon: true
+                        });
+                    });
+                </script>
 CHAINE_DE_FIN;
     }
 }
