9c51efa74ca073fac332fb25d5ad56dd3dcb3e88
Vardoom <paul.vardon@polytechnique.edu>
2017-04-14 19:04:23 +0200

    Vardon Sign In & Submit

--- StreetArtProject/content/content_signin.php
+++ StreetArtProject/content/content_signin.php
@@ -1,14 +1,73 @@
-<div>
-    Vous pouvez vous identifier !!!
+<style>
+    p {
+        margin-top: 0px;
+    }
+
+    fieldset {
+        margin-bottom: 15px;
+        padding: 10px;
+    }
+
+    legend {
+        padding: 0px 3px;
+        font-weight: bold;
+        font-variant: small-caps;
+    }
+
+    label {
+        width: 110px;
+        display: inline-block;
+        vertical-align: top;
+        margin: 6px;
+    }
+
+    em {
+        font-weight: bold;
+        font-style: normal;
+        color: #f00;
+    }
+
+    input:focus {
+        background: #eaeaea;
+    }
+
+    input, textarea {
+        width: 249px;
+    }
+
+    textarea {
+        height: 100px;
+    }
+
+    select {
+        width: 254px;
+    }
+
+    input[type=checkbox] {
+        width: 10px;
+    }
+
+    input[type=submit] {
+        width: 150px;
+        padding: 10px;
+    }
+
+    .centrage{
+        text-align: center;
+    }
+</style>
+
+<div class="centrage">
+    <h2>Formulaire d'identification</h2>
+    <form class="form-inline" action="index.php?page=welcome&todo=login" method="post">
+        <p><i>Complétez le formulaire. Les champs marqué par </i><em>*</em> sont <em>obligatoires</em></p><br>
+        <fieldset>
+            <legend>Identifiez-vous</legend>
+            <label class="sr-only" for="exampleInputEmail3">Login</label>
+            <input type="text" class="form-control" id="exampleInputEmail3" placeholder="Login" name="login" required>
+            <label class="sr-only" for="exampleInputPassword3">Password</label><br>
+            <input type="password" class="form-control" name="mdp" id="exampleInputPassword3" placeholder="Mot de passe" required>
+        </fieldset>
+        <p><button type="submit" class="btn btn-default">Sign in</button></p>
+    </form>
 </div>
-<form class="form-inline" action="index.php?page=welcome&todo=login" method="post" >
-    <div class="form-group">
-        <label class="sr-only" for="exampleInputEmail3">Login</label>
-        <input type="text" class="form-control" id="exampleInputEmail3" placeholder="Login" name="login" required>
-    </div>
-    <div class="form-group">
-        <label class="sr-only" for="exampleInputPassword3">Password</label>
-        <input type="password" class="form-control" name="mdp" id="exampleInputPassword3" placeholder="Mot de passe" required>
-    </div>
-    <button type="submit" class="btn btn-default">Sign in</button>
-</form>
--- StreetArtProject/content/content_submit.php
+++ StreetArtProject/content/content_submit.php
@@ -34,19 +34,7 @@ if (!empty($_FILES['fichier']['tmp_name']) && is_uploaded_file($_FILES['fichier'
         if (move_uploaded_file($_FILES['fichier']['tmp_name'], '/Applications/XAMPP/xamppfiles/htdocs/StreetArtProject2/StreetArtProject/images/' . $_POST['nom'] . $_POST['id'] . '.jpg')) {
             echo "Copie réussie";
             $name = $_POST['nom'].$_POST['id'];
-            $source = imagecreatefromjpeg("images/$name.jpg"); // La photo est la source
-            $destination = imagecreatetruecolor(200, 150); // On crée la miniature vide
-            // Les fonctions imagesx et imagesy renvoient la largeur et la hauteur d'une image
-            $largeur_source = imagesx($source);
-            $hauteur_source = imagesy($source);
-            $largeur_destination = imagesx($destination);
-            $hauteur_destination = imagesy($destination);
-
-            // On crée la miniature
-            imagecopyresampled($destination, $source, 0, 0, 0, 0, $largeur_destination, $hauteur_destination, $largeur_source, $hauteur_source);
-
-            // On enregistre la miniature sous le nom "mini_couchersoleil.jpg"
-            imagejpeg($destination, "miniatures/mini_$name.jpg");
+            Image::createMiniature($name);
         } else {
             echo "Echec de la copie";
         }
